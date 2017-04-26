c3852dbfc14f5109a5b4ff399953142e4ea9c758
Jason Haas <jasonrhaas@gmail.com>
2017-04-26 16:08:26 -0300

    Add /api endpoint and basic HTML

--- application.py
+++ application.py
@@ -1,9 +1,28 @@
 from flask import Flask
 application = Flask(__name__)
 
+
 @application.route('/')
 def hello_world():
-    return 'Hello, World!'
+
+    return 'Please use /api to use the DataNorth API.'
+
+@application.route('/api')
+def api_intro():
+
+    intro = \
+    """
+    <h2> Welcome to the DataNorth API!  </h2>
+    <h4> The following endpoints are available: </h4>
+
+    <ul>
+      <li>/api/crime</li>
+      <li>/api/energy</li>
+      <li>/api/housing</li>
+    </ul>
+    """
+
+    return intro
 
 
 if __name__ == "__main__":
